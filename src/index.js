db7476977e792bee6999ba2c237909730eef2af0
Giuseppe Greco <giuseppegreco2292@gmail.com>
2023-01-17 17:11:24 +0100

    step test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import demoData from './demo-data.json';
 
 function App(props) {
+
+  const [step, setStep] = useState(0);
+
+  function increaseStep(step) {
+    const maxStep = props.kata.steps.length;
+    const nextStep = (step + 1) >= maxStep ? 0 : step + 1;
+
+    console.log(nextStep);
+    setStep(nextStep);
+  }
+
   return (
     <main>
       <h1>Kata Navigator</h1>
       <Instructions name={props.kata.name} />
-      <Solution step={props.kata.steps[0]} />
-      <Navigation />
+      <Solution step={props.kata.steps[step]} />
+      <Navigation onClick={() => increaseStep(step)} step={step}/>
     </main>
   );
 }
@@ -40,6 +51,9 @@ function Navigation(props) {
   return(
     <section>
       <h2>Placeholder for Navigation section</h2>
+      <button
+        onClick={props.onClick}
+      >Current Step: {props.step} - Increase</button>
     </section>
   );
 }
