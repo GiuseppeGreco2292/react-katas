fb4900e715d67bf1ac47429818d181af1b8fd798
Giuseppe Greco <giuseppegreco2292@gmail.com>
2023-01-19 16:08:10 +0100

    404 and navigator as test routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { KataNavigator } from './pages/kata-nav';
+import { Page404 } from './pages/404';
 import './index.css';
 import demoData from './demo-data.json';
-import { KataNavigator } from './kata-nav';
 
 function App(props) {
 
   return (
-    <KataNavigator kata={props.kata} />
+    <Routes>
+      <Route path={"/"} element={<KataNavigator kata={props.kata} />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
   );
 }
 
@@ -25,6 +30,8 @@ const kata = pickKata();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App kata={kata} />
+    <BrowserRouter>
+      <App kata={kata} />
+    </BrowserRouter>
   </React.StrictMode>
 );
